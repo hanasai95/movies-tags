4f82d31c1921ae9ef1cf4cd90e0404ead4daafff
hanasai95 <1220hanasai@gmail.com>
2021-09-27 21:56:25 -0400

    setup basic components and installed dependencies

---
+++ src/components/movieList/MovieList.js
@@ -0,0 +1,11 @@
+import MovieItem from "./MovieItem";
+import React from "react";
+
+export default function MovieList() {
+  return (
+    <div>
+      <MovieItem />
+      MovieList
+    </div>
+  );
+}
---
+++ src/components/searchBar/SearchBar.js
@@ -0,0 +1,5 @@
+import React, { useContext, useState } from "react";
+
+export default function SearchBar() {
+  return <nav>Search Bar</nav>;
+}
