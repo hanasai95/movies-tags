f8aceaaaacddd033fe7c98f1495cc39e8276546e
hanasai95 <1220hanasai@gmail.com>
2021-09-27 22:14:50 -0400

    datafetch success

--- src/components/movieList/MovieList.js
+++ src/components/movieList/MovieList.js
@@ -1,11 +1,23 @@
 import MovieItem from "./MovieItem";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { getTag } from "../../features/tags/tagsSlice";
 import { useSelector } from "react-redux";
-
+import axios from "axios";
 export default function MovieList() {
   const tagState = useSelector(getTag);
+  const [movieData, setMovieData] = useState([]);
+
+  const API_URL = "https://my.api.mockaroo.com/movies.json?key=3bc829b0";
+  useEffect(async () => {
+    async function fetchData() {
+      const response = await axios(API_URL);
+      setMovieData(response.data);
+    }
+    // if (!movieData)
+    fetchData();
+  }, []);
   console.log({ tagState });
+  console.log(movieData);
   return (
     <div>
       <MovieItem />
